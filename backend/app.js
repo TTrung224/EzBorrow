0ced5b6fd1ff88f6f65960dc4fdd1ef086505100
kaisergrimlock <s3891987@rmit.edu.vn>
2022-11-08 16:19:18 +0700

    update

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const students = require('./students')
- 
-app.use(bodyParser.json())
-const url = "mongodb+srv://admin:admin@cluster0.mnwyjpj.mongodb.net/db1?retryWrites=true&w=majority"
-mongoose.connect(url)
- 
-app.use('/students', students)
- 
+require("dotenv").config();
+require("./config/database").connect();
+const express = require("express");
+const bcrypt =  require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const app = express();
+
+app.use(express.json());
+
+// Logic goes here
+
+module.exports = app;
 //api for courses
 app.listen(9000)
