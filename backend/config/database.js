c0d4dffdc205173feb85ba7b5f04d22310d816a3
kaisergrimlock <s3891987@rmit.edu.vn>
2022-11-12 16:27:45 +0700

    register

---
+++ backend/config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { MONGO_URI } =process.env 
+exports.connect = () => {
+    mongoose.connect(MONGO_URI)
+    .then(() => {
+        console.log('Connected to ' + MONGO_URI);
+    }).catch((error) => {
+        console.error('Error connecting to ' + MONGO_URI);
+        console.error(error);
+        process.exit(1);
+    })
+}
--- backend/model/student.js
+++ backend/model/student.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const studentSchema = new mongoose.Schema({
   first_name: { type: String, default: null },
   last_name: { type: String, default: null },
   email: { type: String, unique: true },
