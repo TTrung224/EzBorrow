91a46805a82fc378937364755a60f298525da31e
kaisergrimlock <s3891987@rmit.edu.vn>
2022-11-19 23:11:18 +0700

    search bar update

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import './Navbar.css'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
+const Profile = (
+  <img
+    src={'https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg'}
+    alt="UserName profile image"
+    roundedCircle
+    style={{ width: '40px' }}
+  />
+)
 
 function Navbar() {
   return (
     <div>
          <div className='Main'>
             <div className='profile'>
-                <img src='https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg' height={50}></img>
+              <DropdownButton id="dropdown-item-button" title={Profile} variant="success">
+                <Dropdown.Item as="button">Login</Dropdown.Item>
+                <Dropdown.Item as="button">Logout</Dropdown.Item>
+              </DropdownButton>
             </div>
         </div>
         <div className='search'>
