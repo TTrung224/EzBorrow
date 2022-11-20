aed4b772bc6491077ae7c67bb59020b733c31596
PhamVietHao <89537529+PhamVietHao@users.noreply.github.com>
2022-11-20 15:21:20 +0700

    update login form

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,12 +1,17 @@
 import React from 'react';
-
+import './Login.css';
 export const Login = () => {
     return(
+        <div className="auth-form-container">
         <form>
                 <label for="username">USERNAME</label>
-                <input type="text" placeholder="your Sid" id="username" name="username"></input>
+                <input type="email" placeholder="your Sid" id="username" name="username"></input>
                 <label for="password">PASSWORD</label>
                 <input type="password" placeholder="********"id="password" name="password"></input>
         </form>
+        <button type='submit'>Log In</button>
+        </div>
     )
 }
+
+export default Login
