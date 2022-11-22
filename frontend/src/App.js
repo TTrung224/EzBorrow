5ab638567944306f5fd38f7a4b5114e7e8625b39
kaisergrimlock <s3891987@rmit.edu.vn>
2022-11-22 19:47:48 +0700

    sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./Common/Navbar"
 import Login from './login'
 import Request from './Request/Request'
+import Sidebar from './Common/Sidebar'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="login" element={<Login />} />
         </Routes>
       </Router>
+      <Sidebar/>
       <Request/>
     </div>
   );
