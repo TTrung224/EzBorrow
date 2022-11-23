014fd2c2a474967a628a373591f1c39bdfa81562
kaisergrimlock <s3891987@rmit.edu.vn>
2022-11-23 09:35:00 +0700

    reformatting frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,9 @@
-import Navbar from "./Common/Navbar"
-import Login from './login'
-import Request from './Request/Request'
-import Sidebar from './Common/Sidebar'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
+import Page from './Common/Page'
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path="login" element={<Login />} />
-        </Routes>
-      </Router>
-      <Sidebar/>
-      <Request/>
+      <Page/>
     </div>
   );
 }
