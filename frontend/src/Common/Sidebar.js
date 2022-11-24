511d4206a567234a5542c34314c1aa2c7e825afd
kaisergrimlock <s3891987@rmit.edu.vn>
2022-11-24 14:31:56 +0700

    Update Sidebar.js

--- frontend/src/Common/Sidebar.js
+++ frontend/src/Common/Sidebar.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import './Sidebar.css'
-import {useState} from 'react'
 import {LinkContainer} from 'react-router-bootstrap';
 
 
 function Sidebar() {
-  const [active1, setActive1] = useState('sideBarItem')
-  const [active2, setActive2] = useState('sideBarItem')
-
   return (
       <div className="sideBarContainer">
         <LinkContainer to={"/dashboard"}>
-          <div className='nonActive' onClick={e => {setActive1('sideBartItemActive'); setActive2('sideBarItem')}}>Dashboard</div>
+          <div className='nonActive'>Dashboard</div>
         </LinkContainer>
         <LinkContainer to={"/request"}>
-          <div className='nonActive' onClick={e => {setActive2('sideBarItemActive'); setActive1('sideBarItem')}}>Request</div>
+          <div className='nonActive'>Request</div>
         </LinkContainer>
       </div>
   )
