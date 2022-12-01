65a44cc2698faf37cd206cd05796dd56177a8245
Trung Tran <s3891724@rmit.edu.vn>
2022-12-01 22:02:48 +0700

    component search, schema enum, borrower email auto

--- backend/routes/component.js
+++ backend/routes/component.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const componentController = require('../controllers/componentController');
 const technician_auth = require('../middleware/technician_auth');
 const verifyToken = require('../middleware/verifyToken');
+
 router.post('/', technician_auth, componentController.create);
 router.put('/:id', technician_auth,componentController.update); // req.body = {updateBorrowed, updateComponent}
 router.delete('/:id', technician_auth, componentController.delete);
+router.get('/search', verifyToken, componentController.getByName);
 router.get('/:id', verifyToken, componentController.getOne);
 
 router.get('/', verifyToken, componentController.getAll);
