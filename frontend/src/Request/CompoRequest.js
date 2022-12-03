0643007d57ca83d3b0b2442bc2abf234841bb89b
kaisergrimlock <s3891987@rmit.edu.vn>
2022-12-03 11:27:39 +0700

    Update CompoRequest.js

--- frontend/src/Request/CompoRequest.js
+++ frontend/src/Request/CompoRequest.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import './CompoRequest.css'
 import axios from 'axios'
+import {useState, useEffect} from 'react'
 
 function CompoRequest() {
     const img1 = 'http://mlab.vn/image/data/Bai%20viet%20ky%20thuat/Arduino/bai%202%20Nhung%20dieu%20co%20ban/ArduinoUnoR3.jpg'
     const number = 1
-    axios.get('http://localhost:4000/component/')
+   const [auth, setAuth] = useState()
+   useEffect(() => {
+    const auth = localStorage.getItem('token');
+    if (auth) {
+     setAuth(auth);
+    }
+  }, []);
+
+  const config = {
+        headers: {
+            "Content-type": "application/json",
+            "Authorization": `Bearer ${auth}`,
+        },
+    };
+
+    console.log('auth', auth)
+
+
+
+    axios.get('http://localhost:4000/component/', { headers: {"Authorization" : `Bearer ${auth}`} })
+    .then((res) => {
+        console.log("res:", res)
+    })
 
     return (
         <div className='compo-container'>
