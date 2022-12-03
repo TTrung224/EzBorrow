37c1e82f0f323cf501ef1c86788bc538a6bdd32a
kaisergrimlock <s3891987@rmit.edu.vn>
2022-12-03 11:14:00 +0700

    Update CompoRequest.js

--- frontend/src/Request/CompoRequest.js
+++ frontend/src/Request/CompoRequest.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './CompoRequest.css'
+import axios from 'axios'
 
 function CompoRequest() {
     const img1 = 'http://mlab.vn/image/data/Bai%20viet%20ky%20thuat/Arduino/bai%202%20Nhung%20dieu%20co%20ban/ArduinoUnoR3.jpg'
     const number = 1
-    
+    axios.get('http://localhost:4000/component/')
+
     return (
         <div className='compo-container'>
             <div className='compo-item'>
