4d0d1ef17b64899ba19dd18d988eeb950e3260fe
s3877653 <hotranminhkhoi@gmail.com>
2022-12-08 12:32:37 +0700

    add some css

--- frontend/src/Dashboard/TechicianList.js
+++ frontend/src/Dashboard/TechicianList.js
@@ -23,35 +23,78 @@ function List() {
             load();
     },[]);
 
-
-return(
-    <div className="card-container">
-        {data.map(item =>
-        <div className="request-card">
-            <div className="card-item-info">
-                    <div className="card-info">
-                        <p>Request date: {item.createdAt}</p>
-                        <p>Borrower email: {item.borrower_email}</p>
-                        <p>Pickup date: {item.pickup_date}</p>
-                        <p>Expected return day: {item.expected_return_date}</p>
-                        <p>Actual return day: {item.actual_return_date}</p>
+    function DisplayBtn(item){
+        if(item.student_status =="waiting"){
+                return(
+                    <div className="card-btns">
+                        <button className="cancel-btn">Cancel</button>
+                        <button className="approve-btn">Approve</button>
                     </div>
-                    <div className="car-item-list">
-                        <p>item list: </p>
-                        {item.component_list.map(component =>
-                            <ul>
-                                <li>{component.name}</li>
-                                <li>{component.amount}</li>
-                            </ul>)}
-                    </div>    
-            </div>
-            <div className="card-btns">
-                <button className="cancel-btn">Cancel</button>
-                <button className="approve-btn">Approve</button>
-                <button className="pickup-btn">Pick up</button>
-                <button className="return-btn">Return</button>
-            </div>
-        </div>)}
-   </div> 
+                )
+            }
+        else if(item.student_status =="ready"){
+            return(
+                <div className="card-btns">
+                    <button className="cancel-btn">Cancel</button>
+                    <button className="pickup-btn">Pick up</button>
+                    <button className="return-btn">Return</button> 
+                </div>
+            )
+        }
+        else if(item.student_status =="picked up"){
+            return(
+                <div className="card-btns">
+                    <button className="return-btn">Return</button>  
+                </div>
+            )
+        }
+        else if(item.student_status =="returned"){
+            return(
+                <div className="card-btns">
+                </div>
+            )
+        }
+        else if(item.student_status =="canceled"){
+            return(
+                <div className="card-btns">
+                </div>
+            )
+        }
+    }
+    return(
+        <div className="card-container">
+            {data.map(item =>
+            <div className="request-card">
+                <div className="card-item-info">
+                        <div className="card-info">
+                            <p>Request date: {item.createdAt}</p>
+                            <p>Borrower email: {item.borrower_email}</p>
+                            <p>Pickup date: {item.pickup_date}</p>
+                            <p>Expected return day: {item.expected_return_date}</p>
+                            <p>Actual return day: {item.actual_return_date}</p>
+                            <p>Student status: {item.student_status}</p>
+                            <p>Technician stuatus: {item.technician_status}</p>
+                        </div>
+                        <div className="car-item-list">
+                            <p>item list: </p>
+                            {item.component_list.map(component =>
+                                <ul>
+                                    <li>{component.name}</li>
+                                    <li>{component.amount}</li>
+                                </ul>)}
+                        </div>    
+                </div>
+             
+                    {DisplayBtn(item)}
+                
+                    
+                
+                
+                
+
+            </div>)}
+        </div> 
+      
+        
 )}
 export default List
