02058029492df0cd1a273e966f0a45514f411e07
Trung Tran <s3891724@rmit.edu.vn>
2022-12-16 10:00:14 +0700

    update lecturer request list with two types

--- frontend/src/Dashboard/LecturerList.js
+++ frontend/src/Dashboard/LecturerList.js
@@ -10,6 +10,8 @@ const format = 'DD MMM, yyyy';
 function TeacherList() {
     console.log("this is lecturer list")
     const [data, setData] = useState([])
+    const [type, setType] = useState("lecturer") 
+    // type: "other" "lecturer"
 
     const Axios = axios.create({
         baseURL: 'http://localhost:4000/',
@@ -17,20 +19,26 @@ function TeacherList() {
     })
 
     useEffect(() => {
+        console.log(type);
+        console.log("useEffect")
         const load = async () => {
             const accountResponse = await Axios.get("/account/getAccount");
             console.log(accountResponse.data);
             const userEmail = accountResponse.data.email;
-
-            const response = await Axios.get('request/lecturer/' + userEmail);
+            var response;
+            if(type == "lecturer"){
+                response = await Axios.get('request/myrequest');
+            } else if(type == "other"){
+                response = await Axios.get('request/lecturer/' + userEmail);
+            }
             var data = await response.data;
             console.log(data);
             setData(data);
         };
             load();
-    },[]);
+    },[type]);
 
-    function DisplayBtn(item){
+    function DisplayOtherBtn(item){
         if(item.lecturer_status == "pending" ){
             return(
                 <div className="card-btns">
@@ -40,39 +48,57 @@ function TeacherList() {
             )
         }
     }
+
+    function DisplayLecturerBtn(item){
+        if(!(item.technician_status == "approved" || item.technician_status == "canceled")){
+            return(
+                <div className="card-btns">
+                  <button onClick={() => requestBtnHandler("cancel", item._id)} className="cancel-btn">Cancel</button>
+                </div>
+            )}   
+    }
+
     return(
-        <div className="card-container">
-            {[...data].reverse().map(request =>
-            <div className="request-card">
-                <div className="card-item-info">
-                    <div className="card-info">
-                        <p>Request date: {Moment(new Date(request.createdAt)).format(format)}</p>
-                        <p>Borrower email: {request.borrower_email}</p>
-                        <p>Pickup date: {Moment(new Date(request.pickup_date)).format(format)}</p>
-                        <p>Expected return day: {Moment(new Date(request.expected_return_date)).format(format)}</p>
-                        <p>Actual return day: 
-                            {request.hasOwnProperty("actual_return_date") &&
-                                " " + Moment(new Date(request.actual_return_date)).format(format)}
-                            {!request.hasOwnProperty("actual_return_date") &&
-                                " not yet returned"}</p>
-                        <p>Lecturer status: {request.lecturer_status}</p>
-                        <p>Technician status: {request.technician_status}</p>
-                        <p>Student status: {request.student_status}</p>
-                    </div>
-                    <div className="car-item-list">
-                        <p>item list: </p>
-                        {request.component_list.map(component =>
-                            <ul>
-                                <li>{component.name} &emsp; x {component.amount}</li>
-                            </ul>)}
-                        <p>note: </p>
-                        <p>{request.note}</p>
+        <div>
+            <div>
+                <button className='type-btn my-requests' onClick={()=>setType("lecturer")}>My Request</button>
+                <button className='type-btn others-requests' onClick={()=>setType("other")}>Others' Request</button>
+            </div>
+
+            <div className="card-container">
+                {[...data].reverse().map(request =>
+                <div className="request-card">
+                    <div className="card-item-info">
+                        <div className="card-info">
+                            <p>Request date: {Moment(new Date(request.createdAt)).format(format)}</p>
+                            <p>Borrower email: {request.borrower_email}</p>
+                            <p>Pickup date: {Moment(new Date(request.pickup_date)).format(format)}</p>
+                            <p>Expected return day: {Moment(new Date(request.expected_return_date)).format(format)}</p>
+                            <p>Actual return day: 
+                                {request.hasOwnProperty("actual_return_date") &&
+                                    " " + Moment(new Date(request.actual_return_date)).format(format)}
+                                {!request.hasOwnProperty("actual_return_date") &&
+                                    " not yet returned"}</p>
+                            <p>Lecturer status: {request.lecturer_status}</p>
+                            <p>Technician status: {request.technician_status}</p>
+                            <p>Student status: {request.student_status}</p>
+                        </div>
+                        <div className="car-item-list">
+                            <p>item list: </p>
+                            {request.component_list.map(component =>
+                                <ul>
+                                    <li>{component.name} &emsp; x {component.amount}</li>
+                                </ul>)}
+                            <p>note: </p>
+                            <p>{request.note}</p>
+                        </div>    
                     </div>    
-                </div>    
-                    {DisplayBtn(request)}
-            </div>)}
-        </div> 
-      
-        
-)}
+                        {(type=="other") && DisplayOtherBtn(request)}
+                        {(type=="lecturer") && DisplayLecturerBtn(request)}
+                </div>)}
+            </div> 
+        </div>
+    )
+
+}
 export default TeacherList
