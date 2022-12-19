90321964fa9dde5d61f1e3f4df8c97ff93ede081
s3877653 <hotranminhkhoi@gmail.com>
2022-12-20 02:16:52 +0700

    fix responsive bug

--- frontend/src/Common/Sidebar.js
+++ frontend/src/Common/Sidebar.js
@@ -5,12 +5,12 @@ import {LinkContainer} from 'react-router-bootstrap';
 var isClose = true;
 function openClose(){ 
   if(isClose){
-    document.querySelector(".dashboardClose").className = "dashboardOpen";
-    document.querySelector(".requestClose").className = "requestOpen";
+    document.querySelector(".dashboardClose").classList.add("dashboardOpen") ;
+    document.querySelector(".requestClose").classList.add("requestOpen");
     isClose = false;
   }else if(!isClose){
-    document.querySelector(".dashboardOpen").className = "dashboardClose";
-    document.querySelector(".requestOpen").className = "requestClose";
+    document.querySelector(".dashboardClose").classList.remove("dashboardOpen") ;
+    document.querySelector(".requestClose").classList.remove("requestOpen") ;
     isClose =true;
   }
 }
@@ -19,7 +19,6 @@ function Sidebar() {
   return (
       <div className="sideBarContainer" onClick={openClose}>
         <button className='sideBarCloseOpentBtn' >sidebar</button>
-        <div></div>
         <LinkContainer to={"/dashboard"}>
           <div className='nonActive dashboardClose'>Dashboard</div>
         </LinkContainer>
