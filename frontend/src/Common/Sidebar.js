75a690a5af0bb6d9999fb8982bc36225f06e5086
s3877653 <hotranminhkhoi@gmail.com>
2022-12-20 01:52:41 +0700

    add side bar responsive

--- frontend/src/Common/Sidebar.js
+++ frontend/src/Common/Sidebar.js
@@ -2,15 +2,29 @@ import React from 'react'
 import './Sidebar.css'
 import {LinkContainer} from 'react-router-bootstrap';
 
+var isClose = true;
+function openClose(){ 
+  if(isClose){
+    document.querySelector(".dashboardClose").className = "dashboardOpen";
+    document.querySelector(".requestClose").className = "requestOpen";
+    isClose = false;
+  }else if(!isClose){
+    document.querySelector(".dashboardOpen").className = "dashboardClose";
+    document.querySelector(".requestOpen").className = "requestClose";
+    isClose =true;
+  }
+}
 
 function Sidebar() {
   return (
-      <div className="sideBarContainer">
+      <div className="sideBarContainer" onClick={openClose}>
+        <button className='sideBarCloseOpentBtn' >sidebar</button>
+        <div></div>
         <LinkContainer to={"/dashboard"}>
-          <div className='nonActive'>Dashboard</div>
+          <div className='nonActive dashboardClose'>Dashboard</div>
         </LinkContainer>
         <LinkContainer to={"/request"}>
-          <div className='nonActive'>Request</div>
+          <div className='nonActive requestClose'>Request</div>
         </LinkContainer>
       </div>
   )
