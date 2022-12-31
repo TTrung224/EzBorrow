8cfed9eb13ac4a86cc0e8f753fe6e01904cf98d8
Trung Tran <s3891724@rmit.edu.vn>
2022-12-31 10:20:14 +0700

    test

--- frontend/src/Context/serverURLContext.js
+++ frontend/src/Context/serverURLContext.js
@@ -1,6 +1,6 @@
 // do not mind this file
 import axios from 'axios';
-export const backendUrl = 'http://localhost:4500'
+export const backendUrl = 'https://ezborrow-server.vercel.app'
 // export const backendUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:4500': 'https://ezborrow-server.vercel.app/'
 export const axiosSetting = axios.create({
     baseURL: `${backendUrl}/`,
