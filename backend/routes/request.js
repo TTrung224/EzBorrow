439515cfae07804f663d9e80d967b1ecd312dce5
Trung Tran <s3891724@rmit.edu.vn>
2022-12-31 10:27:24 +0700

    bug

--- backend/routes/request.js
+++ backend/routes/request.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const requestController = require('../controllers/requestController');
+const requestController = require('../controllers/RequestController');
 const verifyToken = require('../middleware/verifyToken');
 const technician_auth = require('../middleware/technician_auth');
 
