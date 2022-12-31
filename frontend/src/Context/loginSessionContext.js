72dd8f37acb9f4ebc2096036f3bc12c934c0ad68
Trung Tran <s3891724@rmit.edu.vn>
2022-12-31 17:07:15 +0700

    test

--- frontend/src/Context/loginSessionContext.js
+++ frontend/src/Context/loginSessionContext.js
@@ -60,13 +60,16 @@ const AuthContextProvider = ({children}) => {
     //login
     const LoginContext = async userForm => {
         try {
+            const cookies = new Cookies();
 
             const res = await axios.post(`${backendUrl}/account/login`, userForm,{withCredentials: true})
             
-
             if(res.status === 200){
+                console.log(200)
                 const token = res.data.token;
-                Cookies.set('token', token, { path: '/', maxAge: 7200 });
+                console.log(token)
+                cookies.set('token', token, { path: '/', maxAge: 7200 });
+                console.log("set")
                 setAuth({
                     isAuthenticated: true,
                     user: res.data
@@ -86,10 +89,12 @@ const AuthContextProvider = ({children}) => {
     
     const Logout = async () => {
         try {
+            const cookies = new Cookies();
+
             console.log("logging out")
             const res = await axios.post(`${backendUrl}/account/logout`,{},{withCredentials: true})
             if (res.status === 200) {
-                Cookies.remove("token", { path: '/' });
+                cookies.remove("token", { path: '/' });
                 console.log("logout succcess")
                 setAuth({isAuthenticated: false, user: null})
                 setLect([]);
