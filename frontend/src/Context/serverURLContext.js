131ab812a2090c3d8a897749a3c699149a335101
Trung Tran <s3891724@rmit.edu.vn>
2023-01-01 17:10:06 +0700

    change context

--- frontend/src/Context/serverURLContext.js
+++ frontend/src/Context/serverURLContext.js
@@ -4,8 +4,8 @@ import Cookies from 'js-cookie'
 
 export const cookie = Cookies.get("token");
 
-// export const backendUrl = 'https://ezborrow-server.vercel.app'
-export const backendUrl = 'http://localhost:4500'
+export const backendUrl = 'https://ezborrow-server.vercel.app'
+// export const backendUrl = 'http://localhost:4500'
 // export const backendUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:4500': 'https://ezborrow-server.vercel.app/'
 export const axiosSetting = axios.create({
     baseURL: `${backendUrl}/`,
